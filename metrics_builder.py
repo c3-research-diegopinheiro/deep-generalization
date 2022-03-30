3f7d14e914df6ce42fe12d6d9730a290b7ea020f
UmbertoB <umbertobarrosf@gmail.com>
2022-03-30 14:41:52 -0300

    feature: include classification report generator

--- metrics_builder.py
+++ metrics_builder.py
@@ -1,6 +1,8 @@
 from sklearn.metrics import classification_report, confusion_matrix
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
+note = 'Note that in binary classification, recall of the positive class is also known as “sensitivity”; recall of ' \
+       'the negative class is “specificity”.';
 
 def __get_confusion_matrix(model, test_generator, batch_size):
     predictions = model.predict(test_generator, 300 // batch_size + 1)
@@ -38,6 +40,7 @@ def generate_confusion_matrix(model, test_dataset, batch_size):
 
 
 def generate_classification_report(model, test_dataset, batch_size):
+    print(note)
     if isinstance(test_dataset, str):
         image_gen = ImageDataGenerator(rescale=1 / 255)
         test_generator = image_gen.flow_from_directory(test_dataset, target_size=(200, 200),
