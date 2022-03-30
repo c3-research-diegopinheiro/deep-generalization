31ed1fc4f986b37aab1bf4ccaebc81fb9982f861
UmbertoB <umbertobarrosf@gmail.com>
2022-03-30 14:34:40 -0300

    feature: include classification report generator

--- metrics_builder.py
+++ metrics_builder.py
@@ -18,6 +18,8 @@ def __get_classification_report(model, test_generator, batch_size):
         x.append((pred[0] > 0.5).astype("int32"))
 
     target_names = ['Yes', 'No']
+    print(test_generator.classes)
+    print(x)
     return classification_report(test_generator.classes, x, target_names=target_names)
 
 
