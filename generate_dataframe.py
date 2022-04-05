1007ff92e84724fcafd8aa20e8195f664404fac2
UmbertoB <umbertobarrosf@gmail.com>
2022-04-05 00:19:41 -0300

    main: update plot

--- generate_dataframe.py
+++ generate_dataframe.py
@@ -1,11 +1,12 @@
 import pandas as pd
-from dataset_path import dataset_path
 from pathlib import Path
 
-yesDir = dataset_path + '/train/yes'
+used_dataset = 'DATASET'
+
+yesDir = used_dataset + '/train/yes'
 paths = []
 for ext in ['*.jpg', '*.jpeg', '*.JPG', '*.png']:
-    paths = [*paths, *[path.parts[-3:] for path in Path(dataset_path).rglob(ext)]]
+    paths = [*paths, *[path.parts[-3:] for path in Path(used_dataset).rglob(ext)]]
 
 df = pd.DataFrame(data=paths, columns=['Dataset', 'State', 'Path'])
 df.to_csv('dataframe.csv')
@@ -13,4 +14,4 @@ df.to_csv('dataframe.csv')
 pie = df.groupby(['Dataset']).count().plot(kind='pie', y='State', autopct='%1.0f%%')
 
 fig = pie.get_figure()
-fig.savefig("plots/dataset-pie.png")
+fig.savefig("plots/dataset-division-from-" + used_dataset + ".png")
--- utils/metrics_builder.py
+++ utils/metrics_builder.py
@@ -1,5 +1,6 @@
 from sklearn.metrics import classification_report, confusion_matrix
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
 # import pandas as pd
 # import seaborn as sn
 # import matplotlib.pyplot as plt
@@ -8,33 +9,39 @@ note = 'Note that in binary classification, recall of the positive class is also
        'the negative class is “specificity”.';
 
 
-def __get_confusion_matrix(model, test_generator, batch_size):
-    predictions = model.predict(test_generator, 300 // batch_size + 1)
-    x = []
-    for pred in predictions:
-        x.append((pred[0] > 0.5).astype("int32"))
+def __mount_test_dataset_generator(test_dataset_path, batch_size):
+    image_gen = ImageDataGenerator(rescale=1 / 255)
+    return image_gen.flow_from_directory(test_dataset_path,
+                                         target_size=(200, 200),
+                                         shuffle=False,
+                                         class_mode='binary',
+                                         batch_size=batch_size,
+                                         save_to_dir=None)
 
-    return confusion_matrix(test_generator.classes, x)
+
+def __get_model_predictions(model, test_generator, batch_size):
+    predictions = model.predict(test_generator, 300 // batch_size + 1)
+    all_predictions = []
+    for pred in predictions:
+        all_predictions.append((pred[0] > 0.5).astype("int32"))
+
+    return all_predictions
+
+
+def __get_confusion_matrix(model, test_generator, batch_size):
+    predictions = __get_model_predictions(model, test_generator, batch_size)
+    return confusion_matrix(test_generator.classes, predictions)
 
 
 def __get_classification_report(model, test_generator, batch_size):
-    predictions = model.predict(test_generator, 300 // batch_size + 1)
-    x = []
-    for pred in predictions:
-        x.append((pred[0] > 0.5).astype("int32"))
-
+    predictions = __get_model_predictions(model, test_generator, batch_size)
     target_names = ['Yes', 'No']
-    return classification_report(test_generator.classes, x, target_names=target_names)
+    return classification_report(test_generator.classes, predictions, target_names=target_names)
 
 
-def generate_confusion_matrix(model, test_dataset, batch_size, plot = True):
+def generate_confusion_matrix(model, test_dataset, batch_size):
     if isinstance(test_dataset, str):
-        image_gen = ImageDataGenerator(rescale=1 / 255)
-        test_generator = image_gen.flow_from_directory(test_dataset, target_size=(200, 200),
-                                                       shuffle=False,
-                                                       class_mode='binary',
-                                                       batch_size=batch_size,
-                                                       save_to_dir=None)
+        test_generator = __mount_test_dataset_generator(test_dataset, batch_size)
         print('Confusion Matrix')
         # sn.heatmap([[144, 6], [102, 48]], annot=True, fmt='g')
         print(__get_confusion_matrix(model, test_generator, batch_size))
@@ -47,17 +54,10 @@ def generate_confusion_matrix(model, test_dataset, batch_size, plot = True):
 def generate_classification_report(model, test_dataset, batch_size):
     print(note)
     if isinstance(test_dataset, str):
-        image_gen = ImageDataGenerator(rescale=1 / 255)
-        test_generator = image_gen.flow_from_directory(test_dataset, target_size=(200, 200),
-                                                       shuffle=False,
-                                                       class_mode='binary',
-                                                       batch_size=batch_size,
-                                                       save_to_dir=None)
+        test_generator = __mount_test_dataset_generator(test_dataset, batch_size)
         print('Classification Report')
         print(__get_classification_report(model, test_generator, batch_size))
 
     else:
         print('Classification Report')
         print(__get_classification_report(model, test_dataset, batch_size))
-
-
