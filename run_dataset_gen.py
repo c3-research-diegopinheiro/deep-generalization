15f9ff2bcb944a9c9f164939f591576e6547ce0e
UmbertoB <umbertobarrosf@gmail.com>
2023-04-16 01:22:43 -0300

    refactor: unified the run code

--- run_dataset_gen.py
+++ run_dataset_gen.py
@@ -2,17 +2,19 @@ from builders.dataset_builder import generate_dataset
 import pandas as pd
 from pathlib import Path
 
-used_dataset = 'dataset/default'
+def run(used_dataset):
+    yesDir = f'{used_dataset}/train/yes'
+    paths = []
+    for ext in ['*.jpg', '*.jpeg', '*.JPG', '*.png']:
+        paths = [*paths, *[path.parts[-3:] for path in Path(used_dataset).rglob(ext)]]
 
-yesDir = f'{used_dataset}/train/yes'
-paths = []
-for ext in ['*.jpg', '*.jpeg', '*.JPG', '*.png']:
-    paths = [*paths, *[path.parts[-3:] for path in Path(used_dataset).rglob(ext)]]
+    df = pd.DataFrame(data=paths, columns=['Dataset', 'State', 'Path'])
+    df.to_csv('dataset/dataframe.csv')
 
-df = pd.DataFrame(data=paths, columns=['Dataset', 'State', 'Path'])
-df.to_csv('dataset/dataframe.csv')
+    for dataset_kind in ['train', 'test']:
+        for noise_amount in [0, .1, .2, .3, .4, .5, .6, .7, .8, .9]:
+            dataset_name = f'{dataset_kind}_{noise_amount}'
+            generate_dataset(dataset_name, dataset_kind, noise_amount)
 
-for dataset_kind in ['train', 'test']:
-    for noise_amount in [0, .1, .2, .3, .4, .5, .6, .7, .8, .9]:
-        dataset_name = f'{dataset_kind}_{noise_amount}'
-        generate_dataset(dataset_name, dataset_kind, noise_amount)
+if __name__ == '__main__':
+    run('dataset/default')
